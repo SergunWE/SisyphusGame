76a59529326029a48cfe3cbcd4338da3e57498ef
Sergei Shubenkov <sergunwe152@gmail.com>
2023-08-13 14:59:33 +0300

    Added next level

--- Assets/Scripts/Initializables/DebugSaveInfoSetter.cs
+++ Assets/Scripts/Initializables/DebugSaveInfoSetter.cs
@@ -6,10 +6,14 @@
     public class DebugSaveInfoSetter : MonoBehaviourInitializable
     {
         [SerializeField] private SaveInfo saveInfo;
+
+        private static bool _set;
         
         public override void Initialize()
         {
+            if(_set) return;
             saveInfo.LastSaveTimeTicks = 1;
             LocalYandexData.Instance.DebugSetPlayerData(saveInfo);
+            _set = true;
         }
     }
---
+++ Assets/Scripts/Other/NextLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using YandexSDK.Scripts;
+
+namespace SkibidiRunner.Managers
+{
+    public class NextLevel : MonoBehaviour
+    {
+        public void GoToNext()
+        {
+            LocalYandexData.Instance.SaveInfo.LevelNumber++;
+        }
+
+        public void DebugLoadNextLevel()
+        {
+            SceneManager.LoadSceneAsync(1);
+        }
+    }
+}
