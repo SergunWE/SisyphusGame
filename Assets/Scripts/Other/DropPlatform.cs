fca51e13d2891acf7c887c40f2f875b2091e1bca
Sergei Shubenkov <sergunwe152@gmail.com>
2023-08-19 17:37:29 +0300

    New obstacles have been added. Small fixes.

---
+++ Assets/Scripts/Other/DropPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace SkibidiRunner.Managers
+{
+    public class DropPlatform : MonoBehaviour
+    {
+        [SerializeField] private Animation objectAnimation;
+        [SerializeField, Range(0,1)] private float speed;
+
+        private void Awake()
+        {
+            objectAnimation["DropPlatform"].speed = speed;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+            objectAnimation.Stop();
+            objectAnimation.Play();
+        }
+    }
+}
