3522a95e1eeddd3b79adbb85476ebb6543ee1508
Sergei Shubenkov <sergunwe152@gmail.com>
2023-09-01 00:43:10 +0300

    Fix continue with stone

--- Assets/Scripts/Other/GameContinuer.cs
+++ Assets/Scripts/Other/GameContinuer.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace SkibidiRunner.Managers
 {
     public class GameContinuer : MonoBehaviour
     {
+        [SerializeField] private Transform direction;
+        [SerializeField] private Vector3 recoveryOffset; 
+        
         public void RollbackGame()
         {
             if (GameInfo.Instance.StoneFall)
@@ -15,8 +19,24 @@ namespace SkibidiRunner.Managers
 
         private void RollbackStone()
         {
-            GameInfo.Instance.MainPlatform.position = Vector3.Scale(GameInfo.Instance.StoneLastPoint,
-                new Vector3(0, 1, 1));
+            Vector3 targetPosition = new Vector3(
+                0,
+                GameInfo.Instance.StoneLastPoint.y,
+                GameInfo.Instance.StoneLastPoint.z
+            );
+
+            // Calculate the required distance to move along the forwardDirection
+            var position = GameInfo.Instance.MainPlatform.position;
+            var forward = direction.forward;
+            float distanceToMove = Vector3.Dot(targetPosition - position, forward);
+
+            // Move the platform along the forwardDirection to the target position
+            position += forward * distanceToMove;
+            GameInfo.Instance.MainPlatform.position = position + recoveryOffset;
+
+
+            // GameInfo.Instance.MainPlatform.position = Vector3.Scale(GameInfo.Instance.StoneLastPoint,
+            //     new Vector3(0, 1, 1));
 
             var rb = ActiveGameObjectStore.Instance.Stone.GetComponent<Rigidbody>();
             rb.isKinematic = true;
