b9b8b37fdd4c761f9ccd5edae8f949440001fc74
Sergei Shubenkov <sergunwe152@gmail.com>
2023-11-17 00:47:38 +0300

    Change models

--- Assets/Scripts/Other/ItemShopManager.cs
+++ Assets/Scripts/Other/ItemShopManager.cs
@@ -6,10 +6,10 @@ namespace SkibidiRunner.Managers
     public class ItemShopManager : MonoBehaviour
     {
         [SerializeField] private LevelNumberView levelView;
-        
-        public void BuyCoins()
+
+        public void BuyCoins(int coinsCount = 1000)
         {
-            ShopManager.Instance.ChangeCoins(400);
+            ShopManager.Instance.ChangeCoins(coinsCount);
         }
 
         public void SkipLevel()
